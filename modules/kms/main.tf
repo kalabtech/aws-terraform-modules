43e5cdac73bc1321c40452c2268eaa5825f07a42
kalabtech <235978118+kalabtech@users.noreply.github.com>
2026-03-17 05:59:20 +0000

    refactor(kms): kms module

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -5,8 +5,8 @@ resource "aws_kms_key" "this" {
   description             = var.description
   deletion_window_in_days = var.deletion_window_days
   enable_key_rotation     = var.enable_key_rotation
-  tags = var.tags
-  
+  tags                    = var.tags
+
   lifecycle {
     prevent_destroy = true
   }
